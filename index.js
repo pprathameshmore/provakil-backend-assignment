eda9aacbd0c83bda9f3c2b324f2aa7ac1b988c07
Prathamesh More <pprathameshmore@outlook.com>
2020-09-19 18:31:22 +0530

    Refactored code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import csvtojsonV2 from "csvtojson";
 import moment from "moment";
 
@@ -24,11 +23,9 @@ const arrayToObject = (array) =>
   }, {});
 
 let _budget = arrayToObject(budget);
-console.log(_budget);
-console.log(budgetForAll);
 
 const budgetDefaultRules = _budget;
-const budgetDefaultRulesForALl = budgetForAll;
+const budgetDefaultRulesForAll = budgetForAll;
 
 const getMonth = (date) => {
   return moment(date, "DD-MM-YYYY").toDate().getMonth() + 1;
@@ -43,118 +40,64 @@ const validateInvestments = (investments) => {
       const monthOfInvestment = getMonth(investment.Date);
 
       if (_budget[investment["Sector"]]) {
+        //Continue for same month
         if (monthOfInvestment === previousMonth) {
           if (
             investment["Sector"].Sector === _budget[investment["Sector"].Sector]
           ) {
-            console.log("Found investment in", investment["Sector"]);
             //Compare
-            //Fix bug here
-            console.log(
-              "Total investment -------------------------------- ",
-              totalInvestmentForMonth
-            );
-            console.log(
-              "Rule amount --------------------------------------",
-              budgetDefaultRules[investment["Sector"]].Amount
-            );
-
-            console.log(
-              "Rule amount --------------------------------------",
-              budgetDefaultRulesForALl
-            );
-
-            if (budgetDefaultRules[investment["Sector"]] === "FinTech") {
-              console.log("Got it");
-            }
-
             if (
               (investment["Amount"] <= _budget[investment["Sector"]].Amount &&
                 totalInvestmentForMonth <=
-                  budgetDefaultRulesForALl["Month"].Amount) ||
+                  budgetDefaultRulesForAll["Month"].Amount) ||
               totalInvestmentForMonth <= budgetDefaultRules["FinTech"].Amount
             ) {
-              console.log("Month of investment", monthOfInvestment);
-              console.log("Valid", index);
               //Do
               totalInvestmentForMonth += parseInt(investment["Amount"]);
               _budget[investment["Sector"]].Amount =
                 _budget[investment["Sector"]].Amount - investment["Amount"];
-
-              console.log("Remaining", _budget[investment["Sector"]].Amount);
-              console.log("Investment in this month", totalInvestmentForMonth);
-              console.log("=================================================");
               previousMonth = monthOfInvestment;
-              console.log("Before month", previousMonth);
             } else {
-              console.log("Error ()()(((((((((((((((((((((((((((((((((((((");
-              console.log("Not valid", index);
               output.push(index + 1);
             }
           }
         } else {
           //Reset all rules
-          console.log(
-            "Started ====================================== Different Month"
-          );
           totalInvestmentForMonth = 0;
           _budget = budgetDefaultRules;
-          budgetForAll = budgetDefaultRulesForALl;
+          budgetForAll = budgetDefaultRulesForAll;
           if (
             investment["Sector"].Sector === _budget[investment["Sector"].Sector]
           ) {
-            console.log("Found investment in", investment["Sector"]);
             //Compare
-
-            console.log("```````````````````````````````````````````````");
-            console.log("Total investment", totalInvestmentForMonth);
-            console.log("Rule", budgetDefaultRulesForALl["Month"]);
             if (
               investment["Amount"] <= _budget[investment["Sector"]].Amount &&
               totalInvestmentForMonth <=
-                budgetDefaultRulesForALl["Month"].Amount
+                budgetDefaultRulesForAll["Month"].Amount
             ) {
-              console.log("Month of investment", monthOfInvestment);
-              console.log("Valid", index);
               //Do
               totalInvestmentForMonth += parseInt(investment["Amount"]);
               _budget[investment["Sector"]].Amount =
                 _budget[investment["Sector"]].Amount - investment["Amount"];
-
-              console.log("Remaining", _budget[investment["Sector"]].Amount);
-              console.log("Investment in this month", totalInvestmentForMonth);
-              console.log("=================================================");
               previousMonth = monthOfInvestment;
-              console.log("Before month", previousMonth);
             } else {
-              console.log("Not valid", index);
               output.push(index + 1);
             }
           }
         }
       } else {
-        //Search in common budget (budgetForALl) which are not present in rule
-        console.log(investment);
+        //Search in common budget (budgetForAll) which are not present in rule
         totalInvestmentForMonth = 0;
         _budget = budgetDefaultRules;
-        budgetForAll = budgetDefaultRulesForALl;
-
-        console.log("-----------------------------------");
-        console.log(_budget[investment["Sector"]]);
+        budgetForAll = budgetDefaultRulesForAll;
         if (
           investment.Amount <= budgetForAll["Month"].Amount &&
-          totalInvestmentForMonth <= budgetDefaultRulesForALl["Month"].Amount
+          totalInvestmentForMonth <= budgetDefaultRulesForAll["Month"].Amount
         ) {
-          console.log("Making investment");
-          console.log("Month of investment", monthOfInvestment);
-          console.log("Valid", index);
           //Do investment
-          console.log("Found investment in", investment["Sector"]);
           totalInvestmentForMonth += parseInt(investment["Amount"]);
           budgetForAll["Month"].Amount =
             budgetForAll["Month"].Amount - investment["Amount"];
-          console.log("Remaining", budgetForAll["Month"].Amount);
-          console.log("Investment in this month", totalInvestmentForMonth);
           previousMonth = monthOfInvestment;
         } else {
           output.push(index + 1);
@@ -167,5 +110,3 @@ const validateInvestments = (investments) => {
 
 validateInvestments(investments);
 console.log(output);
-
-/* const monthOfInvestment = moment(investment.Date, "DD-MM-YYYY").toDate().getMonth() + 1; */
